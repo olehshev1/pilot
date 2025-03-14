2edc9d9f05e5209a56433033c9dd58bcf7d643c8
Oleh Shevtsov <olehshev1@gmail.com>
2025-03-14 14:18:26 +0100

    PILOT-12: filtering and caching

--- app/services/projects/index.rb
+++ app/services/projects/index.rb
@@ -2,19 +2,60 @@ module Projects
   class Index < ApplicationService
     attr_reader :user, :projects, :errors
 
-    def initialize(user)
+    CACHE_EXPIRATION = 15.minutes
+
+    def initialize(user, params = {})
       @user = user
+      @params = params
       @errors = []
     end
 
     def call
-      @projects = @user.projects.includes(:user, :tasks)
+      @projects = fetch_cached_projects
 
       self
     end
 
     def success?
       @errors.empty?
+    end
+
+    private
+
+    def fetch_cached_projects
+      Rails.cache.fetch(cache_key, expires_in: CACHE_EXPIRATION) do
+        fetch_projects
+      end
+    end
+
+    def fetch_projects
+      build_query.includes(:user, :tasks)
+    end
+
+    def build_query
+      apply_status_filter(base_query)
+    end
+
+    def base_query
+      @user.projects
+    end
+
+    def apply_status_filter(query)
+      return query unless @params[:task_status].present?
+
+      query.joins(:tasks)
+           .where(tasks: { status: @params[:task_status] })
+           .distinct
+    end
+
+    def cache_key
+      parts = [ 'projects', @user.id, @user.updated_at.to_s ]
+
+      if @params[:task_status].present?
+        parts << "task_status:#{@params[:task_status]}"
+      end
+
+      parts.join(':')
     end
   end
 end
